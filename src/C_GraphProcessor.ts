64bf024ad403d6920462d2975dcbe16385a290a3
Long Vu <redralogro@gmail.com>
2021-06-10 19:18:21 +0700

    WIP

--- src/C_GraphProcessor.ts
+++
@@ -1,9 +0,0 @@
-class node<T> {
-    public id_node: string |null = null; 
-    public next: node<T> | null = null;
-    public weight: number  | null = null;
-    public prev: node<T> | null = null;
-    constructor(public data: T) {
-
-    }
-  }
