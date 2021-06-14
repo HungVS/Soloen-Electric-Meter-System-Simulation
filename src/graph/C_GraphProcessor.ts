2dd4180bb45c1b28f849f1ddd6256b396410b257
Long Vu <redralogro@gmail.com>
2021-06-14 23:35:10 +0700

    get graph method

--- src/graph/C_GraphProcessor.ts
+++ src/graph/C_GraphProcessor.ts
@@ -5,16 +5,18 @@ export class C_GraphProcessor {
     
  initializeGraph (graph: Graph, vertexlist: Vertex[],weight: number[]) {
     let tempadjencyList:IadjencyList[] = [];
+    let weight_index = 0
     for (let i = 0; i < vertexlist.length; i++ ){
         let tempVertex = vertexlist[i]
         let tempadjencyVertices = []
-        for (let j = 0; j < vertexlist[i].adjencyVerticesList.length; j++){
-            tempadjencyVertices.push({vertex :vertexlist[i].adjencyVerticesList[j], weight: 1})
+        for (let j = 0; j < vertexlist[i].adjencyVerticesList.length; j++){ 
+            tempadjencyVertices.push({vertex :vertexlist[i].adjencyVerticesList[j], weight: weight[weight_index]})
+            weight_index += 1;
         }
         tempadjencyList.push ({vertexRoot:tempVertex, adjencyVertices:tempadjencyVertices})
     }
     graph.setAdjacencyList(tempadjencyList)
-    console.log(graph.adjencyList)
+    //console.log(graph.adjencyList)
 };
 
 
@@ -23,10 +25,14 @@ export class C_GraphProcessor {
 }
 
  getGraph  (graph: Graph) {
+    console.log('Tracking graph :')
     for (let i = 0; i < graph.adjencyList.length; i++ ){
-        console.log(graph.adjencyList[i].vertexRoot)
+        console.log('Root of adjacency list :')
+        console.log( graph.adjencyList[i].vertexRoot.id +' <---{')
         for (let j = 0; j < graph.adjencyList[i].adjencyVertices.length; j++){
+            console.log('  { vertex :'+graph.adjencyList[i].adjencyVertices[j].vertex.id +','+'weight: '+graph.adjencyList[i].adjencyVertices[j].weight +' }')
         }
+        console.log('}')
     }
 }
 
